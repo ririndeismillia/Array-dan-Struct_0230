a9de26934c571dce2e357013437463d389e86383
ririndeismillia <ririndeismillia@gmail.com>
2026-04-06 17:47:47 +0700

    commit3

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -25,7 +25,26 @@ int main() {
     cout << "Data index 4 = ";
     cin >> kamu[4];
     cout << endl;
+    // Menampilkan Satu per Satu
+    cout << "Data Pertama = " << kamu[0] << endl;
+    cout << "Data Kedua = " << kamu[1] << endl;
+    cout << "Data Ketiga = " << kamu[2] << endl;
+    cout << "Data Keempat = " << kamu[3] << endl;
+    cout << endl;
 
+    //Mengisi dengan menggunakan perulangan for
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data index ke-" << i << " = ";
+        cin >> kamu[i];
+
+    }
+    cout << endl;
+    // Menampilkan dengan menggunakan perulangan for
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data ke-" << i+1 << " = " << kamu[i] << endl;
+    }
 
 
 }
