5ebf390e27961a930919db64a31015cb5298fff9
ririndeismillia <ririndeismillia@gmail.com>
2026-04-06 18:47:37 +0700

    commit9

--- structNew2.cpp
+++ structNew2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-struct detaialamat
+struct detailAlamat
 {
     string desa;
     string kota;
@@ -10,10 +10,27 @@ struct detaialamat
 struct Orang
 {
     string nama;
-    detaialamat alamat;
+    detailAlamat alamat;
     int umur;
 };
 
-
-
-
+int main()
+{
+    Orang mhs;
+    // Mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Alamat : ";
+    cin >> mhs.alamat.desa;
+    cout << "Kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+    // tampilkan
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat.desa << ", " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
