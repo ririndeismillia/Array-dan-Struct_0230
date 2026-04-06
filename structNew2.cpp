296e0b19ac5b1e88f31d12a867d1f2d7c385fa05
ririndeismillia <ririndeismillia@gmail.com>
2026-04-06 18:34:43 +0700

    commit8

---
+++ structNew2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct detaialamat
+{
+    string desa;
+    string kota;
+};
+
+struct Orang
+{
+    string nama;
+    detaialamat alamat;
+    int umur;
+};
+
+
+
+
