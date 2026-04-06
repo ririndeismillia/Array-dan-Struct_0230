a92f502888f5e39e0e0e5722afce4b298bdc7158
ririndeismillia <ririndeismillia@gmail.com>
2026-04-06 17:36:30 +0700

    commit2

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -7,5 +7,25 @@ int main() {
     int aku[5] = {10,20,30,40,50};
     // deklarasi array tanpa inisialisasi
     int kamu[5];
-    
+    // tampilkan data(membaca data array)
+    cout << "Data pada index 1 = " << aku[1] << endl;
+    // ganti index ke 1(menulis data array)
+    aku[1] = 200;
+    cout << endl;
+    cout << "Data pada index 1 = " << aku[1] << endl;
+    // Mengisis Satu per Satu
+    cout << "Data index 0 = ";
+    cin >> kamu[0];
+    cout << "Data index 1 = ";
+    cin >> kamu[1];
+    cout << "Data ondex 2 = ";
+    cin >> kamu[2];
+    cout << "Data index 3 = ";
+    cin >> kamu[3];
+    cout << "Data index 4 = ";
+    cin >> kamu[4];
+    cout << endl;
+
+
+
 }
